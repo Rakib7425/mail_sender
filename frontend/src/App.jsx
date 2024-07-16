ebd273feacb18784c4252dcff9be87c4ddfe8d79
Rakibul Islam <irakibul026@gmail.com>
2024-07-16 19:01:58 +0530

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { ReactMultiEmail, isEmail } from "react-multi-email";
+import "./assets/style.css";
 
 function App() {
   const [emails, setEmails] = useState([]);
@@ -10,6 +11,7 @@ function App() {
   const [password, setPassword] = useState("auwo rqbq aews wyaq");
   const [content, setContent] = useState("");
   const [status, setStatus] = useState("");
+  const [isSendingMails, setIsSendingMails] = useState(true);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -17,6 +19,7 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSendingMails(true);
     const formData = new FormData();
     formData.append("emails", JSON.stringify(emails));
     formData.append("senderEmail", senderEmail);
@@ -29,7 +32,7 @@ function App() {
 
     try {
       if (emails.length < 1) {
-        return setStatus("No email provided !");
+        return setStatus("No email provided!");
       }
 
       setStatus("Sending emails...");
@@ -44,78 +47,106 @@ function App() {
         }
       );
       setStatus(response.data);
+      setIsSendingMails(false);
     } catch (error) {
       console.error(error);
-      setStatus("An error occurred");
+      setStatus("An error occurred check console for more info !!");
+      setIsSendingMails(false);
     }
   };
 
   return (
     <div className="App">
       <h1>My Mailer</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Sender Email"
-          value={senderEmail}
-          onChange={(e) => setSenderEmail(e.target.value)}
-          required
-          autoComplete="off"
-        />
-        <br />
-        <input
-          autoComplete="off"
-          type="password"
-          placeholder="Email Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="Subject"
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
-          required
-        />
-
-        <br />
-        <textarea
-          placeholder="Email Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          required
-        />
-        <br />
-        <ReactMultiEmail
-          emails={emails}
-          onChange={(_emails) => {
-            setEmails(_emails);
-          }}
-          placeholder="To Email"
-          validateEmail={(email) => isEmail(email)}
-          getLabel={(email, index, removeEmail) => {
-            return (
-              <div data-tag key={index}>
-                {email}
-                <span
-                  className="crossBtn"
-                  data-tag-handle
-                  onClick={() => removeEmail(index)}
-                >
-                  X
-                </span>
-              </div>
-            );
-          }}
-        />
-        <br />
-        <input type="file" onChange={handleFileChange} />
-        <br />
-        <button type="submit">Send Emails</button>
+      <form onSubmit={handleSubmit} className="mailer-form">
+        <div className="form-row">
+          <label>
+            Sender Email:
+            <input
+              type="email"
+              placeholder="Sender Email"
+              value={senderEmail}
+              onChange={(e) => setSenderEmail(e.target.value)}
+              required
+              autoComplete="off"
+            />
+          </label>
+          <label>
+            Email Password:
+            <input
+              autoComplete="off"
+              type="password"
+              placeholder="Email Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Subject:
+            <input
+              type="text"
+              placeholder="Subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <label>
+          Email Content:
+          <textarea
+            placeholder="Email Content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+          />
+        </label>
+        <div className="form-row">
+          <label>
+            Recipient Emails: To Email
+            <ReactMultiEmail
+              emails={emails}
+              onChange={(_emails) => {
+                setEmails(_emails);
+              }}
+              // placeholder="To Email"
+              validateEmail={(email) => isEmail(email)}
+              getLabel={(email, index, removeEmail) => {
+                return (
+                  <div data-tag key={index}>
+                    {email}
+                    <span
+                      className="crossBtn"
+                      data-tag-handle
+                      onClick={() => removeEmail(index)}
+                    >
+                      X
+                    </span>
+                  </div>
+                );
+              }}
+            />
+          </label>
+          <label>
+            Attachment:
+            <input
+              type="file"
+              onChange={handleFileChange}
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel,text/comma-separated-values, text/csv, application/csv"
+            />
+          </label>
+        </div>
+        <div className="form-row">
+          <button type="submit" className="send-button">
+            {isSendingMails && <span className="loader"></span>} Send Emails
+          </button>
+        </div>
       </form>
-      <p>{status}</p>
+      <p className="status-message">{status}</p>
+      <a href="/sample_emails.xlsx" download className="download-link">
+        Download Sample Excel File
+      </a>
     </div>
   );
 }
